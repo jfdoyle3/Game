f5a6b8f2196eb8bd11ae073a0470fa4e13e557b6
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2019-03-30 10:47:03 -0400

    Add code

---
+++ Game/Board.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class Board
+    {
+        int Height = 4;
+        int Width = 2;
+        static String[] parts = { "|", " ", "+", "-" };
+        String wall = parts[0];
+        String space = parts[1];
+        String plus = parts[2];
+        String dash = parts[3];
+
+        public void BuildBoard(int height, int width)
+        {
+              this.Height = height;
+              this.Width = width;
+             Console.WriteLine("{0},{1}", height, width);
+
+
+        
+           
+            for (var vert = 0; vert <= Height+1; vert++)
+             {
+               
+              for (var horz = 0; horz <= Width+1; horz++)
+             {
+                   
+                    if (vert % 2 == 0)
+                    {
+                        Console.Write("{0}{1}", plus, dash);
+                    } else
+                    {
+                        Console.Write("{0}{1}", wall, space);
+                    }
+                }
+                Console.WriteLine();
+             }
+            Console.WriteLine();
+        }    
+ 
+    }
+ }
---
+++ Game/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+           board.BuildBoard(3,2);
+         
+
+        }
+         
+    }
+}
+
